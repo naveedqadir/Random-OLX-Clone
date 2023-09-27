817c0191e11fb0c5a31e7cdd79c4825cd79fede5
naveedqadir <naveedqadir0@gmail.com>
2023-09-28 01:03:18 +0530

    Update index.js

--- client/src/index.js
+++ client/src/index.js
@@ -5,11 +5,21 @@ import App from './App';
 import { GoogleOAuthProvider } from '@react-oauth/google';
 import 'mdb-react-ui-kit/dist/css/mdb.min.css';
 import "@fortawesome/fontawesome-free/css/all.min.css";
-import { ChakraProvider } from '@chakra-ui/react';
+import { ChakraProvider, extendTheme } from '@chakra-ui/react';
+
+// 2. Extend the theme to include custom colors, fonts, etc
+const theme = extendTheme({
+  fonts: {
+    heading: `'Bebas Neue', sans-serif`,
+    body: `'Nunito', sans-serif`,
+    // Optionally, you can define an accent font:
+    accent: `'Pacifico', cursive`,
+  },
+});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <ChakraProvider>
+  <ChakraProvider theme={theme}>
   <GoogleOAuthProvider clientId="792060318576-1mqeijfldhmbfck89vprnv7p1vm6tv6r.apps.googleusercontent.com">
   <React.StrictMode>
     <App />
