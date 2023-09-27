8c1f48b8cdc19b3dd48f6acc383fdb9015885bca
naveedqadir <naveedqadir0@gmail.com>
2023-09-27 06:55:36 +0530

    Update MyChat.js

--- client/src/MyChat.js
+++ client/src/MyChat.js
@@ -66,7 +66,7 @@ export default function MyChat() {
       const fetchData = async () => {
         try {
           const response = await axios.post(
-            `https://random-backend-yjzj.onrender.com/deletechat/previewad/${id}`,
+            `https://random-backend-yjzj.onrender.com/previewad/${id}`,
             {},
             {
               headers: {
